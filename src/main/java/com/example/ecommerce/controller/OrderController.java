5e2ad0979bede475631d8fbcc513cbbea80e43ab
camiii <amirulsyahmi2311@gmail.com>
2024-05-15 15:13:23 +0800

    update OrderController.java

--- src/main/java/com/example/ecommerce/controller/OrderController.java
+++ src/main/java/com/example/ecommerce/controller/OrderController.java
@@ -1,7 +1,13 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.models.Orders;
+import com.example.ecommerce.models.OrdersProduct;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @Controller
 public class OrderController {
@@ -10,4 +16,15 @@ public class OrderController {
 
         return "cart";
     }
+
+//    private Orders orders;
+//
+//    @GetMapping("/cart")
+//    public String displayCart(Model model) throws SQLException {
+//        List<OrdersProduct> items = orders.getOrders();
+//        double total = orders.calcTotal();
+//        model.addAttribute("items", items);
+//        model.addAttribute("total", total);
+//        return "cart"; // Return the name of the Thymeleaf template (cart.html)
+//    }
 }
