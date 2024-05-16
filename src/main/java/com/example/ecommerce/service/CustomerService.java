a39623109b729f25c49ae5fdb063142beb47acdc
quzaimiamirul <quzaimiamirul@gmail.com>
2024-05-16 09:38:47 +0800

    Customer Setting page
    to view the page please go to http://localhost:8080/customer/1

---
+++ src/main/java/com/example/ecommerce/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> getAllCustomer();
+
+    Customer saveCustomer(Customer customer);
+
+    Customer getCustomerById(Integer customer_id);
+
+    Customer updateCustomer(Customer customer);
+
+    void deleteCustomerById(Integer customer_id);
+}
